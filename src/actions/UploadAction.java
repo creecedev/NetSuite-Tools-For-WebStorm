e865324a8455aa4bcd83658b56f0ba48573811d8
Chris Reece <creece@Chriss-MacBook-Pro.local>
2017-01-03 01:43:58 -0500

    Initial coding for background threading when uploading files

--- src/actions/UploadAction.java
+++ src/actions/UploadAction.java
@@ -1,13 +1,9 @@
 package actions;
 
-import com.intellij.openapi.editor.Document;
-import com.netsuite.webservices.platform.messages_2016_2.WriteResponse;
+import com.intellij.openapi.progress.ProgressManager;
+import com.intellij.openapi.progress.util.ProgressWindow;
+import com.intellij.openapi.vcs.changes.RunnableBackgroundableWrapper;
 
-import com.intellij.openapi.ui.MessageType;
-import com.intellij.openapi.ui.popup.Balloon;
-import com.intellij.openapi.ui.popup.JBPopupFactory;
-import com.intellij.openapi.wm.WindowManager;
-import com.intellij.ui.awt.RelativePoint;
 import projectsettings.ProjectSettingsController;
 import netsuite.*;
 
@@ -16,7 +12,8 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.openapi.fileEditor.FileDocumentManager;
+
+import tasks.UploadTask;
 
 import javax.swing.*;
 
@@ -34,7 +31,6 @@ public class UploadAction extends AnAction {
     public void actionPerformed(AnActionEvent e) {
         final Project project = e.getProject();
         final VirtualFile[] files = e.getData(PlatformDataKeys.VIRTUAL_FILE_ARRAY);
-        final ProjectHelper projectHelper = new ProjectHelper();
 
         ProjectSettingsController projectSettingsController = new ProjectSettingsController(project);
 
@@ -46,7 +42,7 @@ public class UploadAction extends AnAction {
             return;
         }
 
-        NSClient nsClient = null;
+        final NSClient nsClient;
 
         try {
             nsClient = new NSClient(nsAccount, currentEnvironment);
@@ -55,83 +51,10 @@ public class UploadAction extends AnAction {
             return;
         }
 
-        processFiles(project, projectHelper.getProjectRootDirectory(project), files, nsClient, projectSettingsController);
-    }
+        RunnableBackgroundableWrapper wrapper = new RunnableBackgroundableWrapper(e.getProject(), "", new UploadTask(project, files, nsClient, projectSettingsController));
+        ProgressWindow progressIndicator = new ProgressWindow(true, project);
+        progressIndicator.setIndeterminate(true);
 
-    private void processFiles(Project project, String projectBaseDirectory, VirtualFile[] files, NSClient nsClient, ProjectSettingsController projectSettingsController) {
-        if (files == null || files.length == 0) {
-            return;
-        }
-
-        for (VirtualFile file : files) {
-            if (file.isDirectory()) {
-                processFiles(project, projectBaseDirectory, file.getChildren(), nsClient, projectSettingsController);
-            } else {
-                saveFile(file);
-
-                final ProjectHelper projectHelper = new ProjectHelper();
-                String projectFilePathFromRootDirectory = projectHelper.getProjectFilePathFromRootDirectory(file, projectBaseDirectory);
-
-                if (projectFilePathFromRootDirectory != null) {
-
-                    String[] foldersAndFile = projectFilePathFromRootDirectory.split("/");
-                    String currentParentFolder =  projectSettingsController.getNsRootFolder();
-
-                    for (int i = 0; i < foldersAndFile.length; i++) {
-                        if (i + 1 != foldersAndFile.length) {
-                            try {
-                                String folderId = nsClient.searchFolder(foldersAndFile[i], currentParentFolder);
-
-                                if (folderId == null) {
-                                    folderId = nsClient.createFolder(foldersAndFile[i], currentParentFolder);
-                                }
-
-                                currentParentFolder = folderId;
-                            } catch (Exception ex) {
-                                JOptionPane.showMessageDialog(null, "Error Searching/Creating Folder", "ERROR", JOptionPane.ERROR_MESSAGE);
-                            }
-                        } else {
-                            try {
-                                String fileId = nsClient.searchFile(foldersAndFile[i], currentParentFolder, projectSettingsController.getNsRootFolder());
-                                WriteResponse response = nsClient.uploadFile(foldersAndFile[i], file.getPath(), fileId, currentParentFolder, "");
-
-                                if (!response.getStatus().isIsSuccess()) {
-                                    JBPopupFactory.getInstance()
-                                            .createHtmlTextBalloonBuilder("<h3>" + foldersAndFile[i] + " Failed To Upload</h3>", MessageType.ERROR, null)
-                                            .setFadeoutTime(3000)
-                                            .createBalloon()
-                                            .show(RelativePoint.getNorthEastOf(WindowManager.getInstance().getIdeFrame(project).getComponent()),
-                                                    Balloon.Position.above);
-
-                                    JOptionPane.showMessageDialog(null, "File: " + foldersAndFile[i] + "\n" +
-                                                                        "NetSuite File Cabinet Parent Folder ID: " + currentParentFolder + "\n" +
-                                                                        "Error Details: " + response.getStatus().getStatusDetail(response.getStatus().getStatusDetail().length - 1).getMessage(),
-                                                                        "FILE UPLOAD ERROR",
-                                                                        JOptionPane.ERROR_MESSAGE);
-                                } else {
-                                    JBPopupFactory.getInstance()
-                                            .createHtmlTextBalloonBuilder("<h3>" + foldersAndFile[i] + " Uploaded Successfully</h3>", MessageType.INFO, null)
-                                            .setFadeoutTime(3000)
-                                            .createBalloon()
-                                            .show(RelativePoint.getNorthEastOf(WindowManager.getInstance().getIdeFrame(project).getComponent()),
-                                                    Balloon.Position.above);
-                                }
-                            } catch (Exception ex) {
-                                JOptionPane.showMessageDialog(null, "Error uploading file", "ERROR", JOptionPane.ERROR_MESSAGE);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    private void saveFile(VirtualFile file) {
-        FileDocumentManager fileDocumentManager = FileDocumentManager.getInstance();
-        Document document = fileDocumentManager.getDocument(file);
-
-        if (fileDocumentManager.isDocumentUnsaved(document)) {
-            fileDocumentManager.saveDocument(document);
-        }
+        ProgressManager.getInstance().runProcessWithProgressAsynchronously(wrapper, progressIndicator);
     }
 }
